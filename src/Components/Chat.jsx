102898e0f6b928981ce88a517ca8906ce8357b89
Adrian Rivadera <adrianerivadera@gmail.com>
2024-04-11 16:48:32 -0300

    agrego pdf

--- src/Components/Chat.jsx
+++ src/Components/Chat.jsx
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 import ChatBot from 'react-simple-chatbot';
 import { ThemeProvider } from 'styled-components';
 import logo from '../assets/logo.webp';
+import logopdf from '../assets/logopdf.png';
+import jsPDF from 'jspdf'
 
 const theme = {
     background: '#f5f8fb',
@@ -20,6 +22,53 @@ export default function Chat() {
     let edad
     let mail
 
+    const currentDate = () => {
+        const months = [
+            'enero', 'febrero', 'marzo', 'abril', 'mayo', 'junio',
+            'julio', 'agosto', 'septiembre', 'octubre', 'noviembre', 'diciembre'
+        ];
+    
+        const date = new Date();
+        const day = date.getDate();
+        const month = months[date.getMonth()];
+        const year = date.getFullYear();
+    
+        return `${day} de ${month} ${year}`;
+    };
+    
+    const fechaActual = currentDate();
+
+
+    const [showDownloadLink, setShowDownloadLink] = useState(false);
+
+    const generatePDF = () => {
+
+        const pdf = new jsPDF();
+        const img = new Image();
+
+        img.src = logopdf;
+    
+        img.onload = () => {
+
+    
+            pdf.addImage(img, 'PNG', 10, 10, 40, 40); 
+            pdf.setFontSize(12);
+            pdf.text(`${fechaActual}`, 160,20)
+
+            
+            pdf.text(`Estimado/a`, 10,70)
+            pdf.setFont(undefined, 'bold');
+            pdf.text(`${nombre}`, 35,70)
+            pdf.setFont(undefined, 'normal');
+            pdf.text(`Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias dicta cum, et nulla nam ipsum animi dolores nobis, consectetur, optio accusamus unde voluptatum! Dolorem inventore recusandae laudantium. Quis laudantium voluptatum quaerat expedita quae totam dignissimos, animi obcaecati sed pariatur magnam illo quas minima ex, placeat unde harum ab fugit earum.`, 10, 80, { maxWidth: 190 })
+    
+
+            pdf.save('informacion_usuario.pdf');
+        };
+    };
+    
+    
+
 
     //Cambio el width segun responsive  
     let width;
@@ -37,7 +86,7 @@ export default function Chat() {
 
         {
             id: 'preg-nombre',
-            message: '¡Bienvenido!¿Cual es tu nombre?',
+            message: '¡Bienvenidoo!¿Cual es tu nombre?',
             trigger: 'nombre'
         },
         {
@@ -88,14 +137,15 @@ export default function Chat() {
                 if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(value)) {
                     mail = value
                     console.log("3 | El mail es: " + mail)
+                    generatePDF();
                     return true;
-                    
+
                 }
                 else {
                     return 'Por favor ingrese un mail valido.';
                 }
             },
-            trigger:"end"
+            trigger: "end"
         },
 
 
@@ -124,6 +174,14 @@ export default function Chat() {
                     floating={false}
                 />
             </ThemeProvider>
+
+            {showDownloadLink && (
+                <div style={{ textAlign: 'center', marginTop: '20px' }}>
+                    <a href="/informacion_usuario.pdf" download="informacion_usuario.pdf">
+                        Descargar PDF
+                    </a>
+                </div>
+            )}
         </>
     );
 }
